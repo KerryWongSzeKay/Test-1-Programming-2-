b60997cd8672b79664ccf4588778bec37f99ac8c
KerryWongSzeKay <61194252+KerryWongSzeKay@users.noreply.github.com>
2020-03-13 01:15:38 +0800

    Add files via upload

--- TestQ3.java
+++ TestQ3.java
@@ -1,12 +1,13 @@
+import java.util.Scanner;
 public class TestQ3
 {
    public static void main (String[]args)
    {
-      String str1 = new String("Hello");
-      System.out.println(str1);
-      String str2 = new String("Hello");
-      System.out.println(str2);
-      
+      Scanner scan=new Scanner(System.in);
+      System.out.print("Enter string 1 :");
+      String str1 = scan.nextLine();
+      System.out.print("Enter string 2 :");
+      String str2 = scan.nextLine();
 
         if(str1.equals(str2))
             System.out.println("They are Equal!");
