321ef30ef688b989898d19d2f6b4ddd96e7bec05
KerryWongSzeKay <61194252+KerryWongSzeKay@users.noreply.github.com>
2020-07-09 16:14:53 +0800

    Add files via upload

---
+++ Quiz2/Animal.java
@@ -0,0 +1,9 @@
+public class Animal{
+   private String name;
+   public static int age;
+
+    public void introduceYourself() {
+      System.out.println("Morr. I am an animal.");
+      System.out.println("and I am " + this.age + " years old.");
+    }
+}
